cf4cc94fa2a3c5986bdaaa6f85dc88b3008b0a22
Simone De Matteis <Simone.De.Matteis-CIC@ibm.com>
2022-11-18 13:06:48 +0100

    - feature: reintroduce structured log (kafka)

---
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/rulesmanager/client/IConfigClient.java
@@ -0,0 +1,14 @@
+package it.finanze.sanita.fse2.ms.gtw.rulesmanager.client;
+
+import java.io.Serializable;
+
+/**
+ * Interface of gtw-config Client.
+ * 
+ * @author Simone Lungarella
+ */
+public interface IConfigClient extends Serializable {
+	
+	String getGatewayName();
+
+}
--- src/main/java/it/finanze/sanita/fse2/ms/gtw/rulesmanager/logging/LoggerHelper.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/rulesmanager/logging/LoggerHelper.java
@@ -1,12 +1,14 @@
-/*
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
 package it.finanze.sanita.fse2.ms.gtw.rulesmanager.logging;
 
+import it.finanze.sanita.fse2.ms.gtw.rulesmanager.client.IConfigClient;
 import it.finanze.sanita.fse2.ms.gtw.rulesmanager.dto.LogDTO;
 import it.finanze.sanita.fse2.ms.gtw.rulesmanager.enums.ResultLogEnum;
 import it.finanze.sanita.fse2.ms.gtw.rulesmanager.utility.StringUtility;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
@@ -15,10 +17,24 @@ import java.util.Date;
 
 /** 
  * 
+ * @author vincenzoingenito 
  */ 
 @Service
 @Slf4j
 public class LoggerHelper {
+    
+	Logger kafkaLog = LoggerFactory.getLogger("kafka-logger"); 
+	
+	@Value("${log.kafka-log.enable}")
+	private boolean kafkaLogEnable;
+	
+	@Autowired
+	private IConfigClient configClient;
+	
+	private String gatewayName;
+	
+	@Value("${spring.application.name}")
+	private String msName;
 	
 	/* 
 	 * Specify here the format for the dates 
@@ -31,6 +47,8 @@ public class LoggerHelper {
 	 */
 	public void trace(String message, String operation, 
 			   ResultLogEnum result, Date startDateOperation) {
+		
+		final String gatewayName = getGatewayName();
 
 		LogDTO logDTO = LogDTO.builder().
 				message(message).
@@ -38,44 +56,65 @@ public class LoggerHelper {
 				op_result(result.getCode()).
 				op_timestamp_start(dateFormat.format(startDateOperation)).
 				op_timestamp_end(dateFormat.format(new Date())).
+				gateway_name(gatewayName).
+				microservice_name(msName).
 				build();
 
 		final String logMessage = StringUtility.toJSON(logDTO);
 		log.trace(logMessage);
+
+		if (Boolean.TRUE.equals(kafkaLogEnable)) {
+			kafkaLog.trace(logMessage);
+		}
 	} 
 	
 	public void debug(String message,  String operation,  
 			   ResultLogEnum result, Date startDateOperation) {
-
+		
+			final String gatewayName = getGatewayName();
 			LogDTO logDTO = LogDTO.builder().
 				message(message).
 				operation(operation).
 				op_result(result.getCode()).
 				op_timestamp_start(dateFormat.format(startDateOperation)).
 				op_timestamp_end(dateFormat.format(new Date())).
+				gateway_name(gatewayName).
+				microservice_name(msName).
 				build();
 		
 		final String logMessage = StringUtility.toJSON(logDTO);
 		log.debug(logMessage);
+
+		if (Boolean.TRUE.equals(kafkaLogEnable)) {
+			kafkaLog.debug(logMessage);
+		}
 	}
 
 	public void info(String message, String operation,  
 			ResultLogEnum result, Date startDateOperation) {
-
+		
+			final String gatewayName = getGatewayName();
 			LogDTO logDTO = LogDTO.builder().
 				message(message).
 				operation(operation).
 				op_result(result.getCode()).
 				op_timestamp_start(dateFormat.format(startDateOperation)).
 				op_timestamp_end(dateFormat.format(new Date())).
+				gateway_name(gatewayName).
+				microservice_name(msName).
 				build();
 		
 		final String logMessage = StringUtility.toJSON(logDTO);
 		log.info(logMessage);
+		if (Boolean.TRUE.equals(kafkaLogEnable)) {
+			kafkaLog.info(logMessage);
+		}
 	} 
 	
 	public void warn(String message, String operation,  
 			   ResultLogEnum result, Date startDateOperation) {
+		
+		final String gatewayName = getGatewayName();
 
 		LogDTO logDTO = LogDTO.builder().
 				message(message).
@@ -83,15 +122,23 @@ public class LoggerHelper {
 				op_result(result.getCode()).
 				op_timestamp_start(dateFormat.format(startDateOperation)).
 				op_timestamp_end(dateFormat.format(new Date())).
+				gateway_name(gatewayName).
+				microservice_name(msName).
 				build();
 		
 		final String logMessage = StringUtility.toJSON(logDTO);
 		log.warn(logMessage);
+ 
+		if (Boolean.TRUE.equals(kafkaLogEnable)) {
+			kafkaLog.warn(logMessage);
+		}
+ 
 	} 
 	
 	public void error(String message, String operation,  
 			   ResultLogEnum result, Date startDateOperation) {
-
+		
+		final String gatewayName = getGatewayName();
 
 		LogDTO logDTO = LogDTO.builder().
 				message(message).
@@ -99,9 +146,29 @@ public class LoggerHelper {
 				op_result(result.getCode()).
 				op_timestamp_start(dateFormat.format(startDateOperation)).
 				op_timestamp_end(dateFormat.format(new Date())).
+				gateway_name(gatewayName).
+				microservice_name(msName).
 				build();
 		
 		final String logMessage = StringUtility.toJSON(logDTO);
 		log.error(logMessage);
+
+		if (Boolean.TRUE.equals(kafkaLogEnable)) {
+			kafkaLog.error(logMessage);
+		}
+		
 	}
+
+	/**
+	 * Returns the gateway name.
+	 * 
+	 * @return The GatewayName of the ecosystem.
+	 */
+	private String getGatewayName() {
+		if (gatewayName == null) {
+			gatewayName = configClient.getGatewayName();
+		}
+		return gatewayName;
+	}
+    
 }
