0c1d2456284b0a0dda60ddeef81e3dab8ab25b5d
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2022-12-02 12:26:59 +0100

    fix: Change post construct with async initialize

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/rulesmanager/scheduler/InvokeEDSClientScheduler.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/rulesmanager/scheduler/InvokeEDSClientScheduler.java
@@ -3,23 +3,29 @@
  */
 package it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler;
 
+import static it.finanze.sanita.fse2.ms.gtw.rulesmanager.enums.ActionRes.KO;
+import static it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.actions.base.IActionRetryEDS.retryExecutorOnException;
+import static java.lang.String.format;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 import it.finanze.sanita.fse2.ms.gtw.rulesmanager.config.eds.changeset.ChangesetCFG;
 import it.finanze.sanita.fse2.ms.gtw.rulesmanager.enums.ActionRes;
 import it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.executors.base.ExecutorEDS;
-import it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.executors.impl.*;
+import it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.executors.impl.DictionaryExecutor;
+import it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.executors.impl.FhirStructuresExecutors;
+import it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.executors.impl.SchemaExecutor;
+import it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.executors.impl.SchematronExecutor;
+import it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.executors.impl.XslExecutor;
 import it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.executors.impl.chunk.TermsChunkExecutor;
 import it.finanze.sanita.fse2.ms.gtw.rulesmanager.utility.ProfileUtility;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-
-import static it.finanze.sanita.fse2.ms.gtw.rulesmanager.enums.ActionRes.KO;
-import static it.finanze.sanita.fse2.ms.gtw.rulesmanager.scheduler.actions.base.IActionRetryEDS.retryExecutorOnException;
-import static java.lang.String.format;
 
 /**
  * Invoke EDS Client Scheduler, handles the invocations to EDS Client endpoints.
@@ -48,14 +54,17 @@ public class InvokeEDSClientScheduler {
 
 	@Autowired
 	private DictionaryExecutor dictionary;
+	 
 	
-	@PostConstruct
-	public void postConstruct() {
+	@Async
+	@EventListener(ApplicationStartedEvent.class)
+	void initialize() {
 		if(!profiles.isTestProfile() && !profiles.isDevOrDockerProfile()) {
 			log.debug("[EDS] Executing post construct");
 			action();
 		}
 	}
+
 	
 	@Scheduled(cron = "${eds.scheduler.invoke}")
 	@SchedulerLock(name = "invokeEDSClientScheduler")
