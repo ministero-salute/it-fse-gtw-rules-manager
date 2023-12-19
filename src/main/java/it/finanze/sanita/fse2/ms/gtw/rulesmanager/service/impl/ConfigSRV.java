257d75288f265cf22cfe422756add34133b324d0
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-12-19 16:21:59 +0100

    fix: sync bug

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/rulesmanager/service/impl/ConfigSRV.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/rulesmanager/service/impl/ConfigSRV.java
@@ -52,7 +52,7 @@ public class ConfigSRV implements IConfigSRV {
     public Boolean isControlLogPersistenceEnable() {
         long lastUpdate = props.get(PROPS_NAME_CONTROL_LOG_ENABLED).getKey();
         if (new Date().getTime() - lastUpdate >= DELTA_MS) {
-            synchronized(PROPS_NAME_CONTROL_LOG_ENABLED) {
+            synchronized(Locks.CONTROL_LOG_ENABLED) {
                 if (new Date().getTime() - lastUpdate >= DELTA_MS) {
                     refresh(PROPS_NAME_CONTROL_LOG_ENABLED);
                 }
@@ -94,4 +94,9 @@ public class ConfigSRV implements IConfigSRV {
         }
         integrity();
     }
+
+    private static final class Locks {
+        public static final Object CONTROL_LOG_ENABLED = new Object();
+    }
+
 }
